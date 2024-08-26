ebdaefe4dd4cfc0b1d66df08099f6007091ff00d
Hoffm4ann <diego.a.hoffmann@gmail.com>
2024-08-26 14:19:20 -0300

    t4

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,6 @@
 module.exports = {
-    development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DATABASE_URL,
+    production: {
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
