2892a858e47f7c3acb716f06aea823a2c8523e14
Hoffm4ann <diego.a.hoffmann@gmail.com>
2024-08-26 15:15:14 -0300

    T5

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,20 @@
 module.exports = {
+    development: {
+        username: "neondb_owner",
+        password: "Vem3kHEyo6LX",
+        database: "neondb",
+        host: "ep-lucky-salad-a5aj6sex.us-east-2.aws.neon.tech",
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    },
     production: {
-        use_env_variable: 'DATABASE_URL',
-        dialect: 'postgres',
+        use_env_variable: "DATABASE_URL",
+        dialect: "postgres",
         dialectOptions: {
             ssl: {
                 require: true,
